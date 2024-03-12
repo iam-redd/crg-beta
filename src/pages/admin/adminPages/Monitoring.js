528bedea2ec64ae6fb6846a8124a5a6ea07b356a
iamredd <reddou4@mail.ru>
2024-03-12 17:00:54 +0500

    123

--- src/pages/admin/adminPages/Monitoring.js
+++ src/pages/admin/adminPages/Monitoring.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Monitoring = () => {
     return (
         <div>
-            Monitoring
+            <div>
+                <div>
+                    <h3>Пользователи</h3>
+                    <p>12345</p>
+                </div>
+                <div></div>
+            </div>
         </div>
     );
 };
