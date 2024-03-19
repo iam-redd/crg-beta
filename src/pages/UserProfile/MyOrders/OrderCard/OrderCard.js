898c13cd1b98340c96c22656f6a7f33ebd282b60
Kadyrzhan23 <112863599+Kadyrzhan23@users.noreply.github.com>
2024-03-20 02:59:09 +0500

    Ошибку с карточкой заказа зафиксил

--- src/pages/UserProfile/MyOrders/OrderCard/OrderCard.js
+++ src/pages/UserProfile/MyOrders/OrderCard/OrderCard.js
@@ -17,8 +17,76 @@ export default function OrderCard({ data }) {
       </div>
       <div className={styles.main}>
         <p>Статус: <span className={styles.status}>{data.status}</span></p>
-        <p>Дата заказа: <span className={styles.status}>{data.createdAt}</span></p>
+        <p>Дата заказа: <span className={styles.status}>{data.createdAt.split('').splice(0,10)}</span></p>
         <p>Сумма заказа: <span className={styles.status}>{data.totalPrice}</span></p>
+      </div>
+      <div
+        className={styles.footer}
+        onClick={handleVisible}>
+        <span>
+          {
+            <>
+              {
+                data.listProducts.length
+              }
+              {
+                data.listProducts.length === 1 ? ' товар' :
+                  <>
+                    {
+                      data.listProducts.length < 5 ? ' товара' : ' товаров'
+                    }
+                  </>
+              }
+            </>
+          }
+        </span>
+        <span>
+          {
+            <motion.img
+              src={icon}
+              alt=""
+              className={styles.img}
+              animate={isVisible ? "open" : "closed"}
+              variants={variants}
+            />
+          }
+        </span>
+      </div>
+      <AnimatePresence>
+        {
+          isVisible &&
+          <motion.div
+            initial={{ height: 0 }}
+            animate={{ height: "auto" }}
+            exit={{ height: 0 }}
+            style={{ overflow: 'hidden' }}>
+            <div className={styles.list_wrapper}>
+              {
+                data.listProducts.map((product, index) => {
+                  return (
+                    <Order data={product} key={index} />
+                  )
+                })
+              }
+            </div>
+          </motion.div>
+        }
+      </AnimatePresence>
+    </div>
+
+  )
+}
+
+
+function Order({ data }) {
+  return (
+    <div className={styles.order}>
+      <img src={`${url.backendUrl}/${data.img}`} alt="" />
+      <div className={styles.info}>
+        <p>Найменования: {data.name}</p>
+        <p>Обьём: {data.weight}</p>
+        <p>Цена: {data.price} сум</p>
+        <p>Количество: {data.amount}</p>
       </div>
     </div>
 
--- src/pages/admin/Monitoring/OrderCardMonitiring/OrderCardMonitorng.js
+++ src/pages/admin/Monitoring/OrderCardMonitiring/OrderCardMonitorng.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './OrderCardMonitoring.module.css'
 import OrderStatus from './OrderStatus/OrderStatus'
+import icon from '../../../../assets/icons/iconbottom.png'
+import url from '../../../../default.json'
+import { AnimatePresence , motion} from 'framer-motion'
 export default function OrderCardMonitorng({ data, getAllOrders }) {
-
+    const [isVisible, setVisible] = useState(false)
+  const handleVisible = () => setVisible(!isVisible)
+  const variants = {
+    open: { transform: 'rotate(-180deg' },
+    closed: { transform: 'rotate(0deg' }
+  }
     return (
         <div className={styles.container}>
             <div className={styles.header}>
@@ -12,8 +20,78 @@ export default function OrderCardMonitorng({ data, getAllOrders }) {
                 <p>Статус: <span className={styles.status}>{data.status}</span></p>
                 <p>Дата заказа: <span className={styles.status}>{data.createdAt}</span></p>
                 <p>Сумма заказа: <span className={styles.status}>{data.totalPrice}</span></p>
+                <div
+                    className={styles.footer}
+                    onClick={handleVisible}>
+                    <span>
+                        {
+                            <>
+                                {
+                                    data.listProducts.length
+                                }
+                                {
+                                    data.listProducts.length === 1 ? ' товар' :
+                                        <>
+                                            {
+                                                data.listProducts.length < 5 ? ' товара' : ' товаров'
+                                            }
+                                        </>
+                                }
+                            </>
+                        }
+                    </span>
+                    <span>
+                        {
+                            <motion.img
+                                src={icon}
+                                alt=""
+                                className={styles.img}
+                                animate={isVisible ? "open" : "closed"}
+                                variants={variants}
+                            />
+                        }
+                    </span>
+                </div>
+                <AnimatePresence>
+                    {
+                        isVisible &&
+                        <motion.div
+                            initial={{ height: 0 }}
+                            animate={{ height: "auto" }}
+                            exit={{ height: 0 }}
+                            style={{ overflow: 'hidden' }}>
+                            <div className={styles.list_wrapper}>
+                                {
+                                    data.listProducts.map((product, index) => {
+                                        return (
+                                            <Order data={product} key={index} />
+                                        )
+                                    })
+                                }
+                            </div>
+                        </motion.div>
+                    }
+                </AnimatePresence>
                 <OrderStatus status={data.status} getAllOrders={getAllOrders} id={data._id} />
             </div>
         </div>
     )
+}
+
+
+
+function Order({ data }) {
+    return (
+        <div className={styles.order}>
+            <img src={`${url.backendUrl}/${data.img}`} alt="" />
+            <div className={styles.info}>
+                <p>Найменования: {data.name}</p>
+                <p>Обьём: {data.weight}</p>
+                <p>Цена: {data.price} сум</p>
+                <p>Количество: {data.amount}</p>
+            </div>
+        </div>
+
+    )
+
 }
