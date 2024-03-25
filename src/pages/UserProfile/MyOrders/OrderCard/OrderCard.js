b2de8a639bd7389b2865716db5d6d749e81cd89f
iamredd <reddou4@mail.ru>
2024-03-25 09:08:04 +0500

    order card

--- src/pages/UserProfile/MyOrders/OrderCard/OrderCard.js
+++ src/pages/UserProfile/MyOrders/OrderCard/OrderCard.js
@@ -83,10 +83,10 @@ function Order({ data }) {
     <div className={styles.order}>
       <img src={`${url.backendUrl}/${data.img}`} alt="" />
       <div className={styles.info}>
-        <p>Найменования: {data.name}</p>
-        <p>Обьём: {data.weight}</p>
+        <p>Название: {data.name}</p>
+        <p>Вес: {data.weight}</p>
         <p>Цена: {data.price} сум</p>
-        <p>Количество: {data.amount}</p>
+        <p>Кол-во: {data.amount} шт.</p>
       </div>
     </div>
 
--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -5,7 +5,7 @@ const UserProfile = () => {
 
     return (
         
-        <div className='mx-auto max-w-screen-xl 2xl:max-w-screen-xl py-10 lg:py-2 sm:p-3 ss:py-5'>
+        <div className='mx-auto max-w-screen-xl 2xl:max-w-screen-xl py-10 lg:py-2 sm:p-3'>
             <div className='flex justify-center'>
             </div>
             <div className=''>
