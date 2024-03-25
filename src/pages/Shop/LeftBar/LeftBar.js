2fd64a520193498b04b5196a32fdd32fea5a5307
iamredd <reddou4@mail.ru>
2024-03-25 13:09:38 +0500

    shop

--- src/pages/Shop/LeftBar/LeftBar.js
+++ src/pages/Shop/LeftBar/LeftBar.js
@@ -31,11 +31,10 @@ export default function LeftBar() {
 
   
     return (
-        <div className='h-screen w-full'>
-            <div className='grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
+        <div className='w-full mb-4 p-4'>
+            <div className='grid grid-flow-row grid-rows-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
             
                 <div className={`p-5 ${styles.catcat}`}>
-                
                 Кофе
                 </div>
                 <div className={`p-5 ${styles.catcat}`}>
@@ -55,109 +54,6 @@ export default function LeftBar() {
                 </div>
                
             </div>
-            <Card className="h-full w-full p-4 shadow-xl shadow-blue-gray-900/5">
-                <div className="mb-2 p-4">
-                    <Typography variant="h5" color="blue-gray">
-                        Каталог
-                    </Typography>
-                </div>
-                <List>
-                    <Accordion
-                        open={open === 1}
-                        icon={
-                            <ChevronDownIcon
-                                strokeWidth={2.5}
-                                className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
-                            />
-                        }
-                    >
-                        <ListItem className="p-0" selected={open === 1}>
-                            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
-                                <ListItemPrefix>
-                                    <PresentationChartBarIcon className="h-5 w-5" />
-                                </ListItemPrefix>
-                                <Typography color="blue-gray" className="mr-auto font-normal">
-                                    Кофе
-                                </Typography>
-                            </AccordionHeader>
-                        </ListItem>
-                        <AccordionBody className="py-1">
-                            <List className="p-0">
-                                <ListItem>
-                                    <ListItemPrefix>
-                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                    </ListItemPrefix>
-                                    В зёрнах
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemPrefix>
-                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                    </ListItemPrefix>
-                                    В капсулах
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemPrefix>
-                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                    </ListItemPrefix>
-                                    Дрип-пакеты
-                                </ListItem>
-                            </List>
-                        </AccordionBody>
-                    </Accordion>
-                    <Accordion
-                        open={open === 2}
-                        icon={
-                            <ChevronDownIcon
-                                strokeWidth={2.5}
-                                className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`}
-                            />
-                        }
-                    >
-                        <ListItem className="p-0" selected={open === 2}>
-                            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
-                                <ListItemPrefix>
-                                    <ShoppingBagIcon className="h-5 w-5" />
-                                </ListItemPrefix>
-                                <Typography color="blue-gray" className="mr-auto font-normal">
-                                    Аксессуары
-                                </Typography>
-                            </AccordionHeader>
-                        </ListItem>
-                        <AccordionBody className="py-1">
-                            <List className="p-0">
-                                <ListItem>
-                                    <ListItemPrefix>
-                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                    </ListItemPrefix>
-                                    Воронки
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemPrefix>
-                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                    </ListItemPrefix>
-                                    Весы
-                                </ListItem>
-                            </List>
-                        </AccordionBody>
-                    </Accordion>
-                    <hr className="my-2 border-blue-gray-50" />
-                    <ListItem>
-                        <ListItemPrefix>
-                            <InboxIcon className="h-5 w-5" />
-                        </ListItemPrefix>
-                        Сиропы
-                        <ListItemSuffix>
-                            <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
-                        </ListItemSuffix>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemPrefix>
-                            <UserCircleIcon className="h-5 w-5" />
-                        </ListItemPrefix>
-                        Химия для кофемашин
-                    </ListItem>
-                </List>
-            </Card>
         </div>
     )
 }
