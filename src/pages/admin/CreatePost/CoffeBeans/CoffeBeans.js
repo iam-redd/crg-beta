4ea78e5682c244ec7f4f8928792d254c75b42feb
Kadyrzhan23 <112863599+Kadyrzhan23@users.noreply.github.com>
2024-03-27 20:54:17 +0500

    111UploadImage.component

--- src/pages/admin/CreatePost/CoffeBeans/CoffeBeans.js
+++ src/pages/admin/CreatePost/CoffeBeans/CoffeBeans.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styles from './CoffeBeans.module.css'
-import UploadImage from '../../../../components/UploadImage/UploadImage'
-import icon from '../../../../'
+import UploadImage from '../../../UserProfile/Registration/UploadImage/UploadImage'
+import icon from '../../../../assets/icons/addImage.png'
 export default function CoffeBeans() {
     const [image, setImage] = useState(null)
     const [secondImage, setSecondImage] = useState(false)
@@ -14,8 +14,17 @@ export default function CoffeBeans() {
                     <UploadImage list={image} setList={setImage} />
                     {
                         secondImage ?
-                            <UploadImage list={secondImage} setList={setImage} /> :
-                            <div className={styles.btn}></div>
+                            <UploadImage list={image} setList={setImage} /> :
+                            <div className={styles.btn}>
+                                <img src={icon} className={styles.btnImg} alt="" onClick={()=> setSecondImage(true)} />
+                            </div>
+                    }
+                    {
+                        secondImage && thirdImage ?
+                            <UploadImage list={image} setList={setImage} /> :
+                            <div className={styles.btn}>
+                                <img src={icon} className={styles.btnImg} alt="" onClick={()=> setThirdImage(true)} />
+                            </div>
                     }
                 </div>
             </div>
