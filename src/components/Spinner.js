91d1b8ec8793e2f3d1f150c0629f8ef7cd01c31b
iamredd <reddou4@mail.ru>
2024-04-24 12:26:44 +0500

    123

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,19 +1,19 @@
 //import { Spinner } from "@material-tailwind/react";
-import { CirclesWithBar , TailSpin } from 'react-loader-spinner'
+import { MutatingDots } from 'react-loader-spinner'
 
 export function DefaultSpinner({ width = 80, height = 80 }) {
   // return <Spinner />
-  return <div className='flex justify-center mx-auto w-full'><TailSpin
-    height={`${height}`}
-    width={`${width}`}
-    color="#FF0000"
-    outerCircleColor="#4fa94d"
-    innerCircleColor="#4fa94d"
-    barColor="#4fa94d"
-    ariaLabel="circles-with-bar-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-    visible={true}
+  return <div className='flex justify-center mx-auto w-full h-full'>
+  <MutatingDots
+  visible={true}
+  height="500"
+  width="100"
+  color="#ba181b"
+  secondaryColor="#ba181b"
+  radius="15"
+  ariaLabel="mutating-dots-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
   />
   </div>
 }
