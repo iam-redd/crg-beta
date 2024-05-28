7fb9d16feae364483148dfa353306c1f1aa38890
iamredd <reddou4@mail.ru>
2024-05-28 22:16:26 +0500

    1234567

--- src/pages/others/About.js
+++ src/pages/others/About.js
@@ -6,12 +6,12 @@ const About = () => {
     return (
         <div className="mt-14 md:h-full sm:flex-wrap xl:flex xl:mx-auto xl:max-w-screen-xl 2xl:max-w-screen-2xl">
             <div>
-                <h2 className='text-2xl border-b border-b-blue-gray-400 pb-1'>О нас</h2>
+                <h2 className='text-2xl border-b border-b-blue-gray-400 pb-1 font-bold '>О нас</h2>
                 <p className='mt-10'>Мы обжариваем кофе уже более 5 лет на лучшем в мире оборудовании. Каждую партию
                     отслеживаем на специальном оборудовании, что позволяет создавать стабильно качественный продукт. В
                     свободную продажу поступает товар, прошедший строгий контроль качества.</p>
 
-                <h3 className='text-xl mt-10 '>В нашем ассортименте вы найдете:</h3>
+                <h3 className='text-xl mt-10 font-medium'>В нашем ассортименте вы найдете:</h3>
                 <div className='flex flex-col gap-2 mt-4'>
                     <p>- Простые понятные сорта кофе на каждый день</p>
                     <p>- Редкие и не обычные сорта которые смогут вас удивить</p>
