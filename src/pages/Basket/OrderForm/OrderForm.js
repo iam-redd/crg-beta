1647065bb3e81cf347893b120c8c4e8c873fa7a4
Kadyrzhan23 <112863599+Kadyrzhan23@users.noreply.github.com>
2024-07-29 23:59:02 +0500

    111

--- src/pages/Basket/OrderForm/OrderForm.js
+++ src/pages/Basket/OrderForm/OrderForm.js
@@ -5,11 +5,12 @@ import { Button, Checkbox, Option, Select } from '@material-tailwind/react';
 import { useDispatch, useSelector } from 'react-redux'
 import { cancelBasket } from '../../../store/slices/basketSlice'
 import { Typography } from "@material-tailwind/react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function OrderForm({ totalPrice }) {
+
     const [btnBool, setBtnBool] = useState(false)
-    const [checkBool,setCheckBool] = useState(false)
+    const [checkBool, setCheckBool] = useState(false)
     const [isError, setError] = useState(false)
     const [errorMessage, setMessage] = useState('')
     const dispatch = useDispatch()
@@ -21,6 +22,7 @@ export default function OrderForm({ totalPrice }) {
     async function newOrder(e) {
         try {
             e.preventDefault()
+
             setError(false)
             if (paymentMethod === null) {
                 // setPomoltColor(true)
@@ -46,6 +48,7 @@ export default function OrderForm({ totalPrice }) {
 
                 return
             }
+            setButloading(true)
             const options = { basket, comment, totalPrice, paymentMethod }
             const data = await axios.post('/new-order', options)
             if (data.status === 200) {
@@ -53,20 +56,21 @@ export default function OrderForm({ totalPrice }) {
             } else {
                 throw new Error('Order not created successfully')
             }
+            setButloading(false)
         } catch (error) {
-
+            setButloading(false)
         }
         setBtnBool(false)
     }
 
 
-    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-    const fetchData = async () =>{
-        setButloading(true);
+    // const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    // async function fetchData() {
+    //     setButloading(true);
 
-        await delay(20000);
-        setButloading(false);
-    }
+    //     await delay(20000);
+    //     setButloading(false);
+    // }
 
 
     const handlePomolColor = () => setPomoltColor(false)
@@ -75,34 +79,34 @@ export default function OrderForm({ totalPrice }) {
             {
                 basket.length > 0 && userInfo !== null ?
                     <form className={`py-2  ${styles.form}`} onSubmit={newOrder}>
-                    <div className='p-2 rounded bg-gray-100'>    
-                    <div className='col-span-3 w-auto'>
-                            <Select size="md"
-                                label="Выберите способ оплаты"
-                                onChange={(e) => changePay(e)}
-                                style={{ borderColor: pomolColor ? "red" : '' }}
-                                onClick={handlePomolColor}>
-                                <>{
-                                    userInfo.role !== 'user' && <Option value='Перечислением'>Перечислением</Option>
-                                }</>
-                                <Option value='PayMe'>PayMe</Option>
-                                <Option value='Другое'>Другое</Option>
-                            </Select>
+                        <div className='p-2 rounded bg-gray-100'>
+                            <div className='col-span-3 w-auto'>
+                                <Select size="md"
+                                    label="Выберите способ оплаты"
+                                    onChange={(e) => changePay(e)}
+                                    style={{ borderColor: pomolColor ? "red" : '' }}
+                                    onClick={handlePomolColor}>
+                                    <>{
+                                        userInfo.role !== 'user' && <Option value='Перечислением'>Перечислением</Option>
+                                    }</>
+                                    <Option value='PayMe'>PayMe</Option>
+                                    <Option value='Другое'>Другое</Option>
+                                </Select>
+                            </div>
                         </div>
-                    </div>
                         <div className='flex items-center gap-1'>
-                                <Checkbox 
-                                    onChange={() => {
-                                        setCheckBool(!checkBool)
-                                        setBtnBool(!checkBool)
-                                    }}
-                                />
-                                <span className={styles.texttip}> Согласен с условиями
-                                        <Link to='/others/privacypolicy'>обработки персональных данных</Link>,
-                                        <Link to='/others/payndelivery'>Доставки</Link> и
-                                        <Link to='/others/publicoffer'>Публичной оферты</Link>
-                                        </span>
-                                </div>
+                            <Checkbox
+                                onChange={() => {
+                                    setCheckBool(!checkBool)
+                                    setBtnBool(!checkBool)
+                                }}
+                            />
+                            <span className={styles.texttip}> Согласен с условиями
+                                <Link to='/others/privacypolicy'>обработки персональных данных</Link>,
+                                <Link to='/others/payndelivery'>Доставки</Link> и
+                                <Link to='/others/publicoffer'>Публичной оферты</Link>
+                            </span>
+                        </div>
                         <textarea className={styles.textarea} name='comment' placeholder='Комментария для заказа'></textarea>
                         {
                             isError &&
@@ -126,17 +130,28 @@ export default function OrderForm({ totalPrice }) {
                                 <p className="text-red-500">{errorMessage}</p>
                             </Typography>
                         }
-                        <Button
-                            type='submit'
-                            className='text-center items-center'
-                            color={butloading ? 'blue-gray' : 'red'}
-                            variant='outlined'
-                            disabled={!btnBool}
-                            onClick={fetchData}
-                            loading={butloading}
-                            >{
-                                butloading ? 'ЗАКАЗЫВАЮ...' : 'ЗАКАЗАТЬ'
-                            }</Button>
+
+
+                        <>
+                            {
+                                butloading ?
+                                    <Button
+                                        loading={butloading}
+                                    >
+                                        ЗАКАЗЫВАЮ...
+                                    </Button> :
+                                    <Button
+                                        type='submit'
+                                        className='text-center items-center'
+                                        color={butloading ? 'blue-gray' : 'red'}
+                                        variant='outlined'
+                                        disabled={!btnBool && butloading}
+
+                                    >ЗАКАЗАТЬ
+                                    </Button>
+                            }
+
+                        </>
                     </form> : <p className='text-center' >Для того чтобы оформить свой заказ, пожалуйста войдите в свой аккаунт или зарегистрируйтесь</p>
             }
         </>
